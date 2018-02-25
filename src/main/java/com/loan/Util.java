23540d26974a2e3f846132e003780414f66bff89
Peter Tran <pltran875@gmail.com>
2018-02-25 00:07:07 +0000

    Monthly repayment calculation

--- src/main/java/com/loan/Util.java
+++ src/main/java/com/loan/Util.java
@@ -1,10 +1,13 @@
 package com.loan;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 /**
  * Set of helper methods for the application.
  */
 public class Util {
-    private static final int PERIOD = 36;
+    private static final int MONTHS = 36;
     private static final int INCREMENT = 100;
     private static final int MIN_REQUEST = 1000;
     private static final int MAX_REQUEST = 15000;
@@ -16,6 +19,33 @@ public class Util {
      */
     public static boolean validateLoanRequest(int amount) {
         return (amount % INCREMENT == 0 && amount >= MIN_REQUEST && amount <= MAX_REQUEST);
+    }
+
+    /**
+     * Calculates the monthly repayment based on EMI, for reference see
+     * https://en.wikipedia.org/wiki/Equated_monthly_installment
+     *
+     * @param rate the annual rate
+     * @param amount the requested loan amount
+     *
+     * @return the monthly repayment
+     */
+    public static BigDecimal calculateMonthlyRepayment(BigDecimal rate, int amount) {
+        BigDecimal twelve = new BigDecimal(12);
+        BigDecimal months = new BigDecimal(MONTHS);
+        BigDecimal requested = new BigDecimal(amount);
+
+        // monthly rate
+        BigDecimal mr = rate.divide(twelve, 8, BigDecimal.ROUND_DOWN);
+
+        BigDecimal oneAddMr = BigDecimal.ONE.add(mr);
+
+        BigDecimal a = mr.multiply(oneAddMr.pow(MONTHS));
+        BigDecimal b = (oneAddMr.pow(MONTHS)).subtract(BigDecimal.ONE);
+
+        BigDecimal aDivB = a.divide(b, 8, BigDecimal.ROUND_DOWN);
+
+        return requested.multiply(aDivB).setScale(2, RoundingMode.DOWN);
     }
 
 }
--- src/test/java/com/loan/UtilTest.java
+++ src/test/java/com/loan/UtilTest.java
@@ -2,6 +2,9 @@ package com.loan;
 
 import org.junit.Test;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -18,5 +21,14 @@ public class UtilTest {
         assertFalse(Util.validateLoanRequest(9999));
         assertFalse(Util.validateLoanRequest(900));
         assertFalse(Util.validateLoanRequest(15100));
+    }
+
+    @Test
+    public void testCalculateRepayment() {
+        BigDecimal act = Util.calculateMonthlyRepayment(new BigDecimal(0.069), 1000);
+        act.setScale(2, RoundingMode.DOWN);
+
+        BigDecimal exp = new BigDecimal("30.83");
+        assertTrue(String.format("expected: %s, actual: %s", exp, act), act.equals(exp));
     }
 }
